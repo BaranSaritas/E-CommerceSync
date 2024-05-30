ada2fcffdfef5ce8bae29a5f0d5be768b2a12663
Baran Sarıtaş <baransaritas.developer@gmail.com>
2024-05-30 19:10:45 +0300

    Initial commit

---
+++ src/main/java/com/E_CommerceSync/E_CommerceSync/ECommerceSyncApplication.java
@@ -0,0 +1,13 @@
+package com.E_CommerceSync.E_CommerceSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/E_CommerceSync/E_CommerceSync/ECommerceSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.E_CommerceSync.E_CommerceSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
